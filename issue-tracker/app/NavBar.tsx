6371962099668b0ddbbd987d6a26bc7488ccabcb
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-25 08:49:31 -0600

    built nav bar

---
+++ issue-tracker/app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { MdBugReport } from "react-icons/md";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 borber-b mb-5 px-5 h-14 items-center">
+      <Link href={"/"}>
+        <MdBugReport />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
