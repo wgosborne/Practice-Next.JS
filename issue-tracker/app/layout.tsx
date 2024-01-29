1ee64b2148e4e223728c25989a2c9db93f092cef
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-28 19:51:23 -0600

    customized the theme, tried to get the font family to be the next but it wouldnt so its just the apple

--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -1,11 +1,16 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme appearance="light" accentColor="orange">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
