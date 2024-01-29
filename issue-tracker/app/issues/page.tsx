97a85ed3ae52213a105f63158e78520cc2ba08bf
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-28 19:32:46 -0600

    set up radix

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
   return (
-    <div>Issues Page</div>
-  )
-}
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
