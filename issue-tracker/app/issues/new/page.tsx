79ccb362c82e025d099d040467930bc708a426df
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-02-03 14:30:48 -0600

    implemented client side validation

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -4,18 +4,25 @@ import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { TypeOf, z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -39,6 +46,7 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -46,6 +54,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
         <Button>Submit New Issue</Button>
       </form>
     </div>
--- issue-tracker/app/page.tsx
+++ issue-tracker/app/page.tsx
@@ -3,23 +3,23 @@
 import React, { useEffect, useRef } from "react";
 import Map, { GeolocateControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import type { NextPage } from 'next';
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import type { NextPage } from "next";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-
-export default function Home(){
-
+export default function Home() {
   const mapContainer = useRef<any>(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = "pk.eyJ1Ijoid2dvc2Jvcm5lIiwiYSI6ImNscnljOHVjYzBzdDAyam1wN3E5cnVndnAifQ.uYaESyqnlQZuKkQaCYRAJA" || '';
-    
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoid2dvc2Jvcm5lIiwiYSI6ImNscnljOHVjYzBzdDAyam1wN3E5cnVndnAifQ.uYaESyqnlQZuKkQaCYRAJA" ||
+      "";
+
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/light-v10',
+      style: "mapbox://styles/mapbox/light-v10",
       center: [15.4542, 18.7322], // center map on Chad
-      zoom: 1.8
+      zoom: 1.8,
     });
 
     // Clean up resources when the component is unmounted
@@ -29,6 +29,10 @@ export default function Home(){
   }, []); // Empty dependency array means this effect runs once, similar to componentDidMount
 
   return (
-    <div className="map-container" ref={mapContainer} style={{ width: '100%', height: '100vh' }} />
+    <div
+      className="map-container"
+      ref={mapContainer}
+      style={{ width: "100%", height: "100vh" }}
+    />
   );
-};
+}
